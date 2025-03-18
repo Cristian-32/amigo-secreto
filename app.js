0826ee306eb70c10b4a9864890c015d7ce72c276
Cristian Infante <david.boca32@gmail.com>
2025-03-17 23:57:12 -0300

    crea función y 2 variables

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigos(){
+    let listarAmigo = document.getElementById("amigo");
+    let nombreAmigo = listarAmigo.ariaValueMax.trim();
+}
 
